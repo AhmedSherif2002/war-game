67886fb62b8f3883b7b86fa5cf40250c6cc5ee71
Ahmed <ahmedsherif602@gmail.com>
2024-08-06 22:54:33 +0300

    port number was changed to 3000

--- frontend/app.js
+++ frontend/app.js
@@ -3,7 +3,7 @@ import Map from "./classes/Map.js";
 import BulletsController from "./classes/BulletsController.mjs";
 import Player from "./classes/Player.js";
 
-const socket = io("http://localhost:4000/");
+const socket = io("http://localhost:3000/");
 setTimeout(()=>{
     console.log(socket)
 },1000)
